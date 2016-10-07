16fb44b7030a3d65e63fb78dfc845bdeae7ba2fa
Renaud COLIN <noctis347@gmail.com>
2016-10-07 10:48:36 +0000

    Update bSearchFile.c

--- src/bSearchFile.c
+++ src/bSearchFile.c
@@ -12,41 +12,41 @@ int main(int argc, char** argv) {
         fprintf(stderr,"Error bad argument number"); exit(1);
     }
     if(! strlen(argv[1])) {
-        fprintf(stderr,"Error first argument is null"); exit(11);
+        fprintf(stderr,"Error first argument is null"); exit(2);
     }
     if(! strlen(argv[2])) {
-        fprintf(stderr,"Error second argument is null"); exit(11);
+        fprintf(stderr,"Error second argument is null"); exit(2);
     }
     bSearchFile(argv[1],argv[2][0]);
 }
 
 void bSearchFile(const char* path, const char c) {
     if(strlen(path) == 0 ) {
-        fprintf(stderr,"Error path is null"); exit(2);
+        fprintf(stderr,"Error path is null"); exit(3);
     }
     FILE* file = fopen(path,"r");
     if(file == NULL ) {
-        perror("Couldn't open file"); exit(3);
+        perror("Couldn't open file"); exit(4);
     }
     if(fseek(file,0,SEEK_END)) {
-        perror("Error when calling fseek to the end of file");exit(4);
+        perror("Error when calling fseek to the end of file");exit(5);
     }
     intmax_t end = ftell(file) -1;
     if( end == 1L) {
-        perror("Error when calling ftell to get size");exit(5);
+        perror("Error when calling ftell to get size");exit(6);
     }
     if(fseek(file,0,SEEK_SET)) {
-        perror("Error when calling fseek to the begin of file\""); exit(6);
+        perror("Error when calling fseek to the begin of file\""); exit(7);
     }
     intmax_t begin = 0;
     while(begin < end) {
         intmax_t middle= (begin+end)/2;
         if( fseek(file,middle,SEEK_SET)) {
-            perror("Error when calling fseek to the middle of file");exit(7);
+            perror("Error when calling fseek to the middle of file");exit(8);
         }
         char buff = (char) fgetc(file);
         if(buff == EOF) {
-            perror("End of file reading : EOF reached ");exit(8);
+            perror("End of file reading : EOF reached ");exit(9);
         }
         if(buff < c)
             begin = middle;
@@ -58,6 +58,6 @@ void bSearchFile(const char* path, const char c) {
         }
     }
     if (fclose(file) == EOF) {
-        perror("Error when closing the file");exit(9);
+        perror("Error when closing the file");exit(10);
     }
 }
